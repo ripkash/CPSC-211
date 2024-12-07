2b43d001dbb416517cad1688e124f0285c77886c
ripkash <166418063+ripkash@users.noreply.github.com>
2024-12-06 22:52:33 -0500

    Delete src/stackexcerise/Stack.java

--- src/stackexcerise/Stack.java
+++
@@ -1,8 +0,0 @@
-package stackexcerise;
-public interface Stack<T>
-{
-    void push (T item);
-	// Remove and return top item, return null if stack is empty
-    T pop ();
-    boolean isEmpty ();
-}
