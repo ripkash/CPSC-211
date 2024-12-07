22fa41891066cd869e65248df2ff6048fd3a2c1f
ripkash <166418063+ripkash@users.noreply.github.com>
2024-12-06 22:42:34 -0500

    Add files via upload

---
+++ lola/DormBathroom.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class DormBathroom extends Link{
+	public DormBathroom(Model m) {
+		super.model = m;
+		super.optionlist.add("");
+	}
+	@Override
+	public String getdescription() {
+		String description = "";
+		return description;
+	}
+
+	@Override
+	public ArrayList<String> getOptions() {
+		OptionList[0] = "Check area";
+		OptionList[1] = "Go back to DormLobby";
+		return super.optionlist;
+	}
+
+	@Override
+	public String processChoice(int choice) {
+		switch (choice) {
+		case 1:
+			return"Not here";
+		case 2:
+			model.setStateType(StateType.DormLobby);
+			return "Go back to Dorm Lobby";
+		}
+		return null;
+	}
+
+}
